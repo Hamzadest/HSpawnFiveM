1e3809efe2e7a2638de6b20cbd03f522a6b08906
Hamza <64666220+Hamzadest@users.noreply.github.com>
2021-04-17 23:45:34 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -3,6 +3,26 @@
 --Modification autorisé, mais appropriation intellectuel interdite. Merci de me contacter en cas de problème. 
 --Ce script optimise et désactive certains scénarion et véhicule. 
 
+
+Citizen.CreateThread(function()
+	for i = 1, 13 do
+		EnableDispatchService(i, EnableDispatch)
+	end
+	while true do
+		-- These natives has to be called every frame.
+		SetVehicleDensityMultiplierThisFrame((TrafficAmount/100)+.0)
+		SetPedDensityMultiplierThisFrame((PedestrianAmount/100)+.0)
+		SetRandomVehicleDensityMultiplierThisFrame((TrafficAmount/100)+.0)
+		SetParkedVehicleDensityMultiplierThisFrame((ParkedAmount/100)+.0)
+		SetScenarioPedDensityMultiplierThisFrame((PedestrianAmount/100)+.0, (PedestrianAmount/100)+.0)
+		SetRandomBoats(EnableBoats)
+		SetRandomTrains(EnableTrains)
+		SetVehicleModelIsSuppressed(GetHashKey("Planes"))
+		SetVehicleModelIsSuppressed(GetHashKey("Trains"))
+		Citizen.Wait(0)
+	end
+end)
+
 Citizen.CreateThread(function()
     -- Ici ce sont les scènarios que on désactive. NE SURTOUT PAS AJOUTER DES SCENARION QUI N'EXISTE PAS SINON CRASH
     local SCENARIO_TYPES = {
